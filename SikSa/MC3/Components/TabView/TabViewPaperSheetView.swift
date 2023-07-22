a74a389fd8069c5f0af627e5dc4bb76dbba12267
Yeongmin_Chae <codudals98@naver.com>
2023-07-22 23:58:37 +0900

    [Feat] 생성&수정_MainView
    * 버튼 이미지 메인뷰에 추가
    * 리팩토링

---
+++ SikSa/MC3/Components/TabView/TabViewPaperSheetView.swift
@@ -0,0 +1,31 @@
+//
+//  TabViewPaperSheetView.swift
+//  SikSa
+//
+//  Created by 채영민 on 2023/07/22.
+//
+
+import SwiftUI
+
+struct TabViewPaperSheetView: View {
+    @ObservedObject var viewModel: MC3ViewModel
+    var page: Int
+
+    var body: some View {
+        LazyVGrid(columns: viewModel.columns) {
+            let injections = PersistenceController.shared.injectionsByPositionArray
+            ForEach(page == 0 ? viewModel.leftArray : viewModel.rightArray,
+                    id: \.self) { position in
+                let status = viewModel.getCircleStatus(of: position, using: injections)
+                TabViewCircleView(viewModel: viewModel, position: position, status: status)
+            }
+        }.frame(maxWidth: .infinity)
+    }
+}
+
+struct TabViewPaperSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = MC3ViewModel()
+        TabViewPaperSheetView(viewModel: viewModel, page: 1)
+    }
+}
