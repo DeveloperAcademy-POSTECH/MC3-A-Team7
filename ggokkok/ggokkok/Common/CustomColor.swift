6dc47fbcb3487cb1d77d3260c555d8be43be6015
sei <vanism7557@gmail.com>
2023-07-28 17:43:46 +0900

    feat: Color 에셋, Onboarding view 추가 .. :)

---
+++ ggokkok/ggokkok/Common/CustomColor.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  GgokKok
+//
+//  Created by sei on 2023/07/28.
+//
+
+import Foundation
+
+enum CustomColor: String {
+    case black
+    case blue100
+    case blue200
+    case blue300
+    case gray300
+    case gray400
+    case gray500
+    case longActing
+    case mixedActing
+    case rapidActing
+    case red
+    case white
+}
---
+++ ggokkok/ggokkok/Extension/Color+.swift
@@ -0,0 +1,23 @@
+//
+//  Color+.swift
+//  GgokKok
+//
+//  Created by sei on 2023/07/28.
+//
+
+import SwiftUI
+
+extension Color {
+    static var black: Color { Color(CustomColor.black.rawValue) }
+    static var blue100: Color { Color(CustomColor.blue100.rawValue) }
+    static var blue200: Color { Color(CustomColor.blue200.rawValue) }
+    static var blue300: Color { Color(CustomColor.blue300.rawValue) }
+    static var gray300: Color { Color(CustomColor.gray300.rawValue) }
+    static var gray400: Color { Color(CustomColor.gray400.rawValue) }
+    static var gray500: Color { Color(CustomColor.gray500.rawValue) }
+    static var longActing: Color { Color(CustomColor.longActing.rawValue) }
+    static var mixedActing: Color { Color(CustomColor.mixedActing.rawValue) }
+    static var rapidActing: Color { Color(CustomColor.rapidActing.rawValue) }
+    static var red: Color { Color(CustomColor.red.rawValue) }
+    static var white: Color { Color(CustomColor.white.rawValue) }
+}
